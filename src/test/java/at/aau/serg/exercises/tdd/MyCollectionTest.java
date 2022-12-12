145bfc3d75b9562d8911301f534390745d7cbf5d
Angela Sternig <angelast@edu.aau.at>
2022-12-12 20:19:37 +0100

    testRemove

--- src/test/java/at/aau/serg/exercises/tdd/MyCollectionTest.java
+++ src/test/java/at/aau/serg/exercises/tdd/MyCollectionTest.java
@@ -2,11 +2,12 @@
 //Matrikelnummer: 12003325
 package at.aau.serg.exercises.tdd;
 
-
+import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 
 public class MyCollectionTest
@@ -23,5 +24,28 @@ public class MyCollectionTest
     @Test
     public void testSizeSimple() {
         assertEquals(3,c.size());
+    }
+
+    @Test
+    public void testRemove(){
+        MyCollection c1 = new MyCollection(5);
+        c1.add("1");
+        c1.add("2");
+        c1.add("3");
+
+
+        Assert.assertThrows(IllegalArgumentException.class, () ->
+                c.remove(null));
+        Assert.assertThrows(IllegalArgumentException.class, () ->
+                c.remove("Hallo"));
+
+        c.remove("1");
+        assertEquals(2, c.size());
+        assertNotEquals(c, c1);
+
+        c1.remove("1");
+
+        assertEquals(c, c1);
+
     }
 }
