3b2b051d3f5f60e874f243fa407baf6131dd269b
Angela Sternig <angelast@edu.aau.at>
2022-12-12 20:29:03 +0100

    Added Remove and Empty in Main Class

--- src/main/java/at/aau/serg/exercises/tdd/MyCollection.java
+++ src/main/java/at/aau/serg/exercises/tdd/MyCollection.java
@@ -33,14 +33,35 @@ public class MyCollection {
      * @param s String to remove
      */
     public void remove(String s) throws IllegalArgumentException{
-
+        boolean found = false;
+        String[] arrayCopy = new String[list.length - 1];
+        if (size() <= 0) {
+            throw new IllegalArgumentException();
+        } else{
+            for (int i = 0, j = 0; i < this.list.length - 1; i++) {
+                if (cursor > i) {
+                    if(!list[i].equals(s)){
+                        arrayCopy[j] = list[i];
+                        j++;
+                    } else{
+                        found = true;
+                        cursor--;
+                    }
+                }
+            }
+        }
+        list = arrayCopy;
+        if(!found) {
+            throw new IllegalArgumentException();
+        }
     }
 
     /**
      * Removes all items from the list and initializes a new list
      */
     public void empty() {
-
+        list = null;
+        cursor = 0;
     }
 
 }
