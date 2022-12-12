3d325bcc6372d0de631f0c727931a44d5b613a42
Angela Sternig <angelast@edu.aau.at>
2022-12-12 20:41:55 +0100

    testRemove working

--- src/test/java/at/aau/serg/exercises/tdd/MyCollectionTest.java
+++ src/test/java/at/aau/serg/exercises/tdd/MyCollectionTest.java
@@ -2,52 +2,45 @@
 //Matrikelnummer: 12003325
 package at.aau.serg.exercises.tdd;
 
-
 import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.*;
 
+@RunWith(JUnitPlatform.class)
+public class MyCollectionTest {
+    private MyCollection c;
 
-public class MyCollectionTest
-{
-        private MyCollection c;
+    @BeforeEach
+    public void setup() {
+        c = new MyCollection(5);
+        c.add("1");
+        c.add("2");
+        c.add("3");
+    }
 
-        @BeforeEach
-        public void setup() {
-            c = new MyCollection(5);
-            c.add("1");
-            c.add("2");
-            c.add("3");
-        }
+    @Test
+    public void testSizeSimple() {
+        assertEquals(3,c.size());
+    }
 
-        @Test
-        public void testSizeSimple() {
-            assertEquals(3,c.size());
-        }
+    @Test
+    public void testRemove(){
+        MyCollection c1 = new MyCollection(5);
+        c1.add("1");
+        c1.add("2");
+        c1.add("3");
 
-        @Test
-        public void testRemove(){
-            MyCollection c1 = new MyCollection(5);
-            c1.add("1");
-            c1.add("2");
-            c1.add("3");
+        c1.remove("1");
+        assertEquals(2, c1.size());
+        assertNotEquals(c, c1);
 
+        c.remove("1");
+        c.remove("2");
 
-            Assert.assertThrows(IllegalArgumentException.class, () ->
-                    c.remove(null));
-            Assert.assertThrows(IllegalArgumentException.class, () ->
-                    c.remove("Hallo"));
-
-            c.remove("1");
-            assertEquals(2, c.size());
-            assertNotEquals(c, c1);
-
-            c1.remove("1");
-
-            assertEquals(c, c1);
-
-        }
+        assertEquals(1, c.size());
+    }
 }
